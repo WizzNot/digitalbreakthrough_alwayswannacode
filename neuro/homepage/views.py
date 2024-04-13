d713113588104ddb849cb909055956e3243a2a09
WizzNot <azaza22122004@gmail.com>
2024-04-13 14:18:56 +0500

    ready image save without html

--- neuro/homepage/views.py
+++ neuro/homepage/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
+import os
+import uuid
 
 
 def home(request):
     template = "homepage/home.html"
     return render(request, template_name=template)
+
+
+def upload(request):
+    if request.method == 'POST' and request.FILES['image']:
+        image_file = request.FILES['image']
+        unique_filename = str(uuid.uuid4()) + os.path.splitext(image_file.name)[-1]
+        fs = FileSystemStorage()
+        fs.save("images/" + unique_filename, image_file)
+
+        return render(request, 'homepage/upload_success.html', {"category": "cat"})
+    return render(request, 'homepage/upload_fail.html')
